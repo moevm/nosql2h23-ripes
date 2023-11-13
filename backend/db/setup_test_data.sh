f65217f91b4692751fe479bb30cde091cfa68045
cyberc001 <cyb3rc001@yandex.ru>
2023-11-13 22:50:35 +0300

    Added MongoDB connection to backend.

--- backend/db/setup_test_data.sh
+++ backend/db/setup_test_data.sh
@@ -1,1 +1,2 @@
+mongosh admin --eval "db.createUser({user: 'ripes_server', pwd: 'a_real_password', roles: ['userAdminAnyDatabase']})"
 mongoimport --db ripes --collection experiments --file test_data.json --mode upsert --jsonArray
--- backend/server/routes.js
+++ backend/server/routes.js
@@ -1,4 +1,35 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const fs = require('fs');
+const log = require('npmlog');
+
+// Set up config
+function generate_mongo_uri(ip, port, user, pwd)
+{
+	return "mongodb://" + user + ":" + pwd + "@" + ip + ":" + port;
+}
+const config = JSON.parse(fs.readFileSync('config.json'));
+
+// Set up MongoDB
+const mongo_client = new MongoClient(generate_mongo_uri(config.db.ip, config.db.port, config.db.user, config.db.pwd));
+const m_db_ripes = mongo_client.db("ripes");
+let m_col_experiments;
+if(m_db_ripes)
+	m_col_experiments = m_db_ripes.collection("experiments");
+else{
+	log.error("mongo", "Database 'ripes' doesn't exist");
+	process.exit(1);
+}
+if(!m_col_experiments){
+	log.error("mongo", "Collection 'experiments' doesn't exist");
+	process.exit(1);
+}
+
+//m_col_experiments.findOne().then((res) => {
+//	console.log(res);
+//});
+
+// Routing
 const router = express.Router();
 
 router.get("/", (req, res) => {
