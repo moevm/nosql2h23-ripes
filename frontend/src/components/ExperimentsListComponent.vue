c0c9a99d3e2a6730b1593382900652742038c5a3
Artyom <reshotkin.02@mail.ru>
2023-11-18 19:39:07 +0300

    Navbar

---
+++ frontend/src/components/ExperimentsListComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="experiments_list"></div>
+  <h1>Список экспериментов</h1>
+</template>
+
+<script>
+export default {
+  name: 'ExperimentsListComponent',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
---
+++ frontend/src/components/StatisticsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="experiments_list">
+        <h1>Статистика экспериментов</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'StatisticsComponent',
+    props: {
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  
+  
